b5b875368fa933ad0c6495553f4d2c68f211a9a7
irmahusein12 <58300968+irmahusein12@users.noreply.github.com>
2019-11-28 23:33:10 +0900

    praktikum4

---
+++ pgame16101/code4.2.py
@@ -0,0 +1,28 @@
+You're using code navigation to jump to definitions or references.
+Learn more or give us feedback
+myvar="Informatika UMMU"
+print(myvar)
+
+# cetak colom 0 (pertama) dari variabel myvar
+print(myvar[0])
+
+# cetak colom 0 dengan keterangan
+print("myvar [0] : ", myvar[0])
+
+# cetak colom awal sampai colom ke 4
+print(myvar[:3])
+
+# cetak colom awal sampai colom ke 4 dengan keterangan
+print("myvar [:3] : ", myvar[:3])
+
+# cetak semua kecuali colom 0 (pertama)
+print(myvar[1:])
+
+# cetak semua kecuali colom 0 (pertama) dengan keterangan
+print("myvar [1:] : ", myvar[1:])
+
+# cetak semua kecuali colom terakhir
+print(myvar[:-1])
+
+# cetak semua kecuali colom terakhir dengan keterangan
+print("myvar [:-1] : ", myvar[:-1])
